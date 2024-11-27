892c995b36025cd77ce62a61fd3916ea729ec2f7
Vivek Nayaran <vivek.n@unilogcorp.com>
2024-11-27 18:17:05 +0530

    Changes

--- opentelemetry/opentelemetry/src/main/java/com/poc/telemetry/open/opentelemetry/controller/HealthController.java
+++ opentelemetry/opentelemetry/src/main/java/com/poc/telemetry/open/opentelemetry/controller/HealthController.java
@@ -3,17 +3,22 @@ package com.poc.telemetry.open.opentelemetry.controller;
 import com.poc.telemetry.open.opentelemetry.service.TelemetryService;
 import io.opentelemetry.api.common.Attributes;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthController {
 
     private final TelemetryService telemetryService;
+    private final KafkaTemplate<String, String> kafkaTemplate;
 
     @Autowired
-    public HealthController(TelemetryService telemetryService) {
+    public HealthController(TelemetryService telemetryService, KafkaTemplate<String, String> kafkaTemplate) {
         this.telemetryService = telemetryService;
+        this.kafkaTemplate = kafkaTemplate;
     }
 
     @GetMapping("/health")
@@ -33,5 +38,23 @@ public class HealthController {
                 },
                 attributes
         );
+    }
+    @PostMapping("/send")
+    public String sendMessage(@RequestParam String message) {
+        Attributes attributes = Attributes.builder()
+                .put("topic", "test-topic")
+                .put("message", message)
+                .build();
+
+        telemetryService.traceOperation(
+                "send_message",
+                "KAFKA",
+                () -> {
+                    kafkaTemplate.send("test-topic", message);
+                },
+                attributes
+        );
+
+        return "Message sent to Kafka";
     }
 }
